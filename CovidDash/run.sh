6a8694aa7d7fde0164f8af797c8cb0abcfd4d0bd
DragosHF <vsim@hellofresh.com>
2021-03-29 16:44:05 +0200

    updated to use either local storage or S3, added logging, changed to python3.8

--- CovidDash/run.sh
+++ CovidDash/run.sh
@@ -4,6 +4,6 @@ if [ "$1" == "local" ]; then
   export S3_ROLE=#enter the S3 role to assume
   export S3_BUCKET=#enter the S3 bucket to be used for ins/outs
 fi
-export ENVIRONMENT="$1"
+export STORAGE="$1"
 python get_data.py
 python dashboard.py
