e078450e18ceed5714c6307581cb3342f656b522
DragosHF <43034022+DragosHF@users.noreply.github.com>
2021-04-02 16:05:51 +0200

    Update run.sh

--- CovidDash/run.sh
+++ CovidDash/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source venv/bin/activate
-if [ "$1" == "local" ]; then
+if [ "$1" == "s3" ]; then
   export S3_ROLE=#enter the S3 role to assume
   export S3_BUCKET=#enter the S3 bucket to be used for ins/outs
 fi
